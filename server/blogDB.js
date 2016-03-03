da593d2872277c317a68b1431f2a7e5bcfda4384
tu6619 <tu6619@my.bristol.ac.uk>
2016-03-03 12:12:06 +0000

    blogdb expanded to include date, author, id. publish blog post returns to dashboard/user page

--- server/blogDB.js
+++ server/blogDB.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var client = require('./client.js');
 var Vision = require('vision');
 var Handlebars = require('handlebars');
@@ -15,14 +17,16 @@ exports.register = function(server, options, next) {
 
     server.route({
         method: 'GET',
-        path: '/',
+        path: '/dashboard/{user}',
         config: {
             handler: function(request, reply) {
+                console.log('a log', reply, request.params.user);
                 function retrieveBlogs(client, callback) {
                     client.LRANGE('posts', 0, -1, function(err, reply) {
                         if (err) {
                             console.log(err);
                         } else {
+
                             callback(reply);
                         }
                     });
@@ -35,19 +39,33 @@ exports.register = function(server, options, next) {
     });
     server.route({
         method: 'GET',
-        path: '/new-post',
+        path: '/dashboard/{user}/new-post',
         config: {
             handler: function(request, reply) {
                 reply.view('new-post');
             }
         }
     });
+
+    // adds date, author, id to db once publish button is clicked
+    //TODO check button compatibility
+    
     server.route({
         method: 'POST',
-        path: '/form',
+        path: '/dashboard/{user}/publish',
         config: {
             handler: function(request, reply) {
+                let dateObj = new Date();
+                let day = dateObj.getDate();
+                let month = dateObj.getMonth() + 1;
+                let year = dateObj.getFullYear();
+                let today = day + '/' + month + '/' + year;
+                console.log(today);
+                request.payload.id = Date.now();
+                request.payload.author = request.params.user;
+                request.payload.date = today;
                 var postObj = JSON.stringify(request.payload);
+
                 console.log('payload______>', request.payload, postObj);
                 function addToDB(client, callback) {
                     client.rpush('posts', postObj, function(err, reply) {
@@ -59,12 +77,21 @@ exports.register = function(server, options, next) {
                     });
                 }
                 addToDB(client, function() {
-                    reply.redirect('/');
+                    reply.redirect('/dashboard/' + request.params.user);
                 });
             }
         }
     });
-    
+    server.route({
+        method: 'GET',
+        path: '/edit-post/id',
+        config: {
+            handler: function(request, reply) {
+
+            }
+        }
+    });
+
     next();
 };
 
